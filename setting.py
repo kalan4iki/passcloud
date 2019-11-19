d7fed580959859fbdd30afa07c8e35e8a408cf12
Vadim <vvk@istra-adm.ru>
2019-11-19 17:45:44 +0300

    Попытки
    Попытки сделайть файл конфигурации приложения

---
+++ setting.py
@@ -0,0 +1,12 @@
+import sys
+
+a = ''
+class mainformset():
+    #code
+    lable = ['ФИО', 'Системы', 'Учетная запись']
+    koltable = 2 #Количество tablewidget
+    tableparams = [[1, 0], [1, 2]] #Параметры lineedit [строка,столбец]
+    lineedit = 1 #Количество lineedit
+    editparams = [[1, 4]]
+    menubarset = 6
+    menubarparams = [['Ctrl+Q' ,'Закрыть приложение.'], []] #Параметры menubarov
