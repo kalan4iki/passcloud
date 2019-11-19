1ca117b2851327128f236311e6c799c07c1cbeb6
Vadim <vvk@istra-adm.ru>
2019-11-19 16:01:10 +0300

    pytest-qt

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 #from qtmain import Mainwin
 import pytest
+from pytestqt import qt_compat
+from pytestqt.qt_compat import qt_api
 from passcloud.qtmain import Mainwin
 from time import sleep
 from PyQt5.QtCore import *
@@ -10,7 +12,7 @@ def test_myapp(qtbot):
     
     qtbot.addWidget(window)
     window.show()
-    #assert qtbot.waitForWindowShown(window) is True
+    assert qtbot.waitForWindowShown(window) is True
     sleep(3)
     #qtbot.mouseClick(window.menubar.actions()[0], Qt.LeftButton)
     assert window.title1.text() == 'ФИО'
